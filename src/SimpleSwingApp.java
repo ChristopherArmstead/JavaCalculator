fb6c8bcf25a38b9e672f134020ad304e0450d64e
Christopher <chrisarmstead648@gmail.com>
2024-05-13 17:01:02 -0400

    sets layout of application and changes fonts

--- src/SimpleSwingApp.java
+++ src/SimpleSwingApp.java
@@ -16,6 +16,7 @@ public class SimpleSwingApp {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Simple Swing Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(false);
 
 
 
@@ -27,45 +28,66 @@ public class SimpleSwingApp {
 
 
         // Create the window (JFrame) and specify the close operation
+
         JMenuBar menuBar = new JMenuBar();
 
         JMenuItem OpenFile = new JMenuItem("Open");
         JMenuItem SaveFile = new JMenuItem("Save");
         JMenu menu = new JMenu("File");
+        menu.setForeground(Color.white);
         menu.add(OpenFile);
         menu.add(SaveFile);
         menuBar.add(menu);
 
         JMenu menu2 = new JMenu("Edit");
+        menu2.setForeground(Color.white);
         JMenuItem Undoitem = new JMenuItem("Undo");
+
         JMenuItem Redoitem = new JMenuItem("Redo");
         menu2.add(Undoitem);
         menu2.add(Redoitem);
 
         menuBar.add(menu2);
 
-        menuBar.add(new JMenu("Help"));
+        JMenu menu3 = new JMenu("Help");
+        menu3.setForeground(Color.white);
+        menuBar.add(menu3);
 
+        JMenu menu4 = new JMenu("About");
+        menu4.setForeground(Color.white);
+        menuBar.add(menu4);
+     //   Font menuBarfont = new Font("default", Font.PLAIN, 12);
 
-        menuBar.add(new JMenu("Exit"));
+        menuBar.setFont(new Font("Monospaced", Font.TRUETYPE_FONT, 12));
+        menuBar.setForeground(Color.WHITE);
+        Color violet = Color.decode("#1a0f4d");
 
+        menuBar.setBackground(violet);
 
+        JTextArea textArea = new JTextArea(4, 10);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        //textArea.setPreferredSize(new Dimension(50,50));
+
+        textArea.setEditable(true);
 
 
         JPanel bluepanel = new JPanel();
         bluepanel.setLayout(new GridLayout(6, 1));
-        bluepanel.setBackground(Color.blue);
+        bluepanel.setBackground(Color.white);
+        bluepanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1, false));
           bluepanel.setPreferredSize(new Dimension(frame.getContentPane().getWidth() / 3 , frame.getContentPane().getHeight()));
 
         JPanel greenpanel = new JPanel();
         greenpanel.setLayout(new BorderLayout());
-       // greenpanel.setPreferredSize(new Dimension(frame.getContentPane().getWidth() - 200, frame.getContentPane().getHeight() /2));
+
         greenpanel.setBackground(Color.green);
 
         JPanel orangepanel = new JPanel();
         orangepanel.setLayout(new GridLayout(5,3));
+        orangepanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1, false));
         orangepanel.setPreferredSize(new Dimension(greenpanel.getWidth() , frame.getContentPane().getHeight() /2 ));
-        orangepanel.setBackground(Color.orange);
+        orangepanel.setBackground(Color.white);
 
 
        // greenpanel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -83,29 +105,77 @@ public class SimpleSwingApp {
        // ImageIcon leftButtonIcon = new ImageIcon("C:\\Users\\chris\\untitled\\resources\\integral-mathematical-sign.gif");
         JButton b1  = new JButton("Intergral");
         b1.setBackground(Color.white);
-
+        Color Black_Rock = Color.decode("#0c042f");
         JButton one = new JButton("1");
+        one.setBorder( BorderFactory.createLineBorder(Color.BLACK,1, true));
+        Font font = new Font("Serif", Font.BOLD, 18);
+        one.setFont(font);
+        one.setBackground(Black_Rock );
+        //one.setContentAreaFilled(false);
+        //one.setOpaque(true);
+       // one.setBackground(Color.WHITE);
         JButton two = new JButton("2");
+        two.setFont(font);
+        two.setBackground(Color.WHITE);
         JButton three = new JButton("3");
+        three.setFont(font);
+        three.setBackground(Color.WHITE);
         JButton four = new JButton("4");
+        four.setFont(font);
+        four.setBackground(Color.WHITE);
         JButton five = new JButton("5");
+        five.setFont(font);
+        five.setBackground(Color.WHITE);
         JButton six = new JButton("6");
+        six.setFont(font);
+        six.setBackground(Color.WHITE);
         JButton seven = new JButton("7");
+        seven.setFont(font);
+        seven.setBackground(Color.WHITE);
         JButton eight = new JButton("8");
+        eight.setFont(font);
+        eight.setBackground(Color.WHITE);
         JButton nine = new JButton("9");
+        nine.setFont(font);
+        nine.setBackground(Color.WHITE);
         JButton zero = new JButton("0");
+        zero.setFont(font);
+        zero.setBackground(Color.WHITE);
         JButton decimal = new JButton(".");
+        decimal.setFont(font);
+        decimal.setBackground(Color.WHITE);
         JButton derivative = new JButton("dy/dx");
-        JButton intergral = new JButton("intergral");
+        derivative.setFont(font);
+        derivative.setBackground(Color.WHITE);
+        JButton intergral = new JButton("Intergral");
+        intergral.setFont(font);
+        intergral.setBackground(Color.WHITE);
         JButton parenthesis = new JButton("( )");
+        parenthesis.setFont(font);
+        parenthesis.setBackground(Color.WHITE);
         JButton Bracket = new JButton("[ ]");
+        Bracket.setFont(font);
+        Bracket.setBackground(Color.WHITE);
         JButton equal = new JButton("=");
+        equal.setFont(font);
+        equal.setBackground(Color.WHITE);
 
         JButton plus = new JButton("+");
+        plus.setFont(font);
+        plus.setBackground(Color.WHITE);
+
         JButton minus = new JButton("-");
+        minus.setFont(font);
+        minus.setBackground(Color.WHITE);
         JButton multiply = new JButton("*");
+        multiply.setFont(font);
+        multiply.setBackground(Color.WHITE);
         JButton divide = new JButton("/");
+        divide.setFont(font);
+        divide.setBackground(Color.WHITE);
         JButton remainder = new JButton("%");
+        remainder.setFont(font);
+        remainder.setBackground(Color.WHITE);
 
 
 
@@ -138,8 +208,10 @@ public class SimpleSwingApp {
 
 
         greenpanel.add(orangepanel,BorderLayout.SOUTH);
+        greenpanel.add(textArea,BorderLayout.CENTER);
         frame.getContentPane().add(bluepanel,BorderLayout.EAST);
         frame.getContentPane().add(greenpanel,BorderLayout.CENTER);
+
 
         frame.setJMenuBar(menuBar);
 
